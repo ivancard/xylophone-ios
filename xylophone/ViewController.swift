a8c46fd6ee8645ffe28338d58920b00452e878d8
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2023-02-18 17:53:09 -0300

    finished app

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  xylophone
 //
@@ -6,14 +5,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: "Sounds/\(soundName)", withExtension: "wav")
+        audioPlayer = try! AVAudioPlayer(contentsOf: url!)
+        audioPlayer?.play()
+    }
+    
+    @IBAction func cKey(_ sender: UIButton) {
+//        playSound(soundName: sender.currentTitle!)
+        guard let key = sender.titleLabel?.text else {return}
+        playSound(soundName: key)
+    }
+    
 }
 
